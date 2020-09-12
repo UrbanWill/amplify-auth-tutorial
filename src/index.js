1af80d5f77828dc08646afdaf5147706d5cb7f53
Will <willian@crowdcast.jp>
2020-09-12 19:32:06 +0900

    added initial authentication

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+
+import "@aws-amplify/ui/dist/style.css";
+
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
